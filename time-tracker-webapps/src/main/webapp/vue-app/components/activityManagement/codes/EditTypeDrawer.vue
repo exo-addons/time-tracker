cd4783adbd4ff99c8650c48dcda6be9a2e2123d0
mkrout <mkrout@exoplatform.com>
2020-11-12 12:36:11 +0100

    Fix Time trackers feedbaks 2

---
+++ time-tracker-webapps/src/main/webapp/vue-app/components/activityManagement/codes/EditTypeDrawer.vue
@@ -0,0 +1,79 @@
+<template>
+<exo-drawer ref="editTypeDrawer" right class="">
+    <template slot="title">
+        Add Type
+    </template>
+    <template slot="content">
+        <div>
+            <v-form ref="form" v-model="valid">
+                <v-row>
+                    <v-label for="code">
+                        Code
+                    </v-label>
+                    <input ref="code" v-model="type.code" type="text" name="code" class="input-block-level ignore-vuetify-classes my-3" />
+                </v-row>
+
+                <v-row>
+                    <v-label for="label">
+                        Label
+                    </v-label>
+                    <input ref="label" v-model="type.label" type="text" name="label" class="input-block-level ignore-vuetify-classes my-3" />
+                </v-row>
+            </v-form>
+        </div>
+    </template>
+    <template slot="footer">
+        <div class="d-flex">
+            <v-spacer />
+            <v-btn class="btn mr-2" @click="cancel()">
+                <template>
+                    Cancel
+                </template>
+            </v-btn>
+            <v-btn class="btn btn-primary" @click="save()">
+                <template>
+                    Save
+                </template>
+            </v-btn>
+        </div>
+    </template>
+</exo-drawer>
+</template>
+
+<script>
+export default {
+
+    data: () => ({
+        defaultItem: {
+            code: '',
+            label: ''},
+        type: {
+            code: '',
+            label: ''
+        }
+    }),
+
+    methods: {
+        save() {
+            this.$emit('save', this.type)
+            this.type = this.defaultItem
+            this.$refs.editTypeDrawer.close()
+        },
+        cancel() {
+            this.type = this.defaultItem
+            this.$refs.editTypeDrawer.close()
+        },
+        open(type) {
+            this.type = type
+            this.$refs.editTypeDrawer.open()
+        },
+
+    }
+}
+</script>
+
+<style>
+.drawerContent {
+    padding: 15px 27px;
+}
+</style>
